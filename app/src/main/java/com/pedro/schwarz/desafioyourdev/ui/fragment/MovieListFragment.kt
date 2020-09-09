a71658fc6bf467c70c623ebe4eb1e81708ed4df2
Pedro Schwarz Rodrigues <pedroschawarz97@gmail.com>
2020-09-09 19:25:00 -0300

    Added navigation

---
+++ app/src/main/java/com/pedro/schwarz/desafioyourdev/ui/fragment/MovieListFragment.kt
@@ -0,0 +1,18 @@
+package com.pedro.schwarz.desafioyourdev.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pedro.schwarz.desafioyourdev.R
+
+class MovieListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie_list, container, false)
+    }
+}
