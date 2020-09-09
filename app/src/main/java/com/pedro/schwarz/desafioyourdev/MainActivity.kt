f69c54432a39fb338c5ef77c4e45a4d2596c76db
Pedro Schwarz Rodrigues <pedroschawarz97@gmail.com>
2020-09-09 17:56:06 -0300

    Initial Commit

---
+++ app/src/main/java/com/pedro/schwarz/desafioyourdev/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pedro.schwarz.desafioyourdev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
